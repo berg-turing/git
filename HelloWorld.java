7b27833096e0b9eb8f5721e6439f51041053ffcb
berg-turing <993450432@qq.com>
2018-07-24 15:26:09 +0800

    modify some file

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,9 @@ public class HelloWorld{
 
     public static void main(String[] args){
 
+        //output Hello World!!!
         System.out.println("Hello World!!!");
+
+        System.out.println("I am here!");
     }
 }
